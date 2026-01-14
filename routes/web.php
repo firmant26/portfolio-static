9e91a2467d35317dff6d560b82dfec87851eb865
firmant26 <andayanabwi@gmail.com>
2026-01-14 20:27:54 +0700

    baru

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
